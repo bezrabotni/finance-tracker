42f15061258a89efcaf78a5a32316328aa372a3b
bezrabotni <dajanovic.damir@gmail.com>
2017-06-23 14:15:54 +0200

    devise sredjivanje

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Include default bootstrap_templates modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable,
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
